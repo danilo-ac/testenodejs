a31b89d075bb629ab7dec301491cea57bdc1a677
danilo-ac <anthony.danilo@outlook.com.br>
2021-11-08 23:26:33 -0300

    :package: initial tests

---
+++ src/tests/SQLCustomerDatabase.test.ts
@@ -0,0 +1,35 @@
+import SQLCustomerDatabase from "../data/SQLCustomerDatabase"
+
+describe("Tests for SQL queries", () => {
+
+    test("Return False for invalid ID", async () => {
+
+        const invalidId = 0.0
+        const testCustomerDB = new SQLCustomerDatabase()
+        const result = await testCustomerDB.isRegisteredCustomerId(invalidId)
+
+        expect(result).toBe(false)
+
+    })
+
+
+    test("Return False for unregistered ID", async () => {
+
+        const testCustomerDB = new SQLCustomerDatabase()
+        const result = await testCustomerDB.isRegisteredCustomerId(991)
+
+        expect(result).toBe(false)
+        
+    })
+
+
+    test("Return True for registered ID", async () => {
+
+        const testCustomerDB = new SQLCustomerDatabase()
+        const result = await testCustomerDB.isRegisteredCustomerId(1)
+
+        expect(result.id).toBe(true)
+
+    })
+
+})
---
+++ src/tests/mocks/SQLCustomerDatabase.ts
@@ -0,0 +1,8 @@
+export class SQLCustomerDatabaseMock {
+
+    public isRegisteredCustomerId = jest.fn(async () => { })
+    public getSalesByCustomerId = jest.fn(async () => { })
+    public postNewCustomer = jest.fn(async () => { })
+    public editCustomer = jest.fn(async () => { })
+
+}
